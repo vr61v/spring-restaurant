3406e038c08b00116099a280008b79395e090c5a
Vr61v <nikita20040503@gmail.com>
2024-06-07 17:32:36 +0300

    feat: create validate request int order

--- order/src/main/java/com/vr61v/model/request/CreateOrderRequest.java
+++ order/src/main/java/com/vr61v/model/request/CreateOrderRequest.java
@@ -1,18 +1,33 @@
 package com.vr61v.model.request;
 
 import com.vr61v.model.OrderState;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Calendar;
 import java.util.Map;
 import java.util.UUID;
 
 public record CreateOrderRequest(
+
+        @NotNull
         UUID userId,
+
+        @NotNull
         UUID restaurantId,
+
+        @NotNull
         Calendar date,
+
+        @NotBlank
         String comment,
+
         OrderState state,
+
+        @NotNull @NotEmpty
         Map<UUID, Integer> products
+
 ) {
 
 }
--- order/src/main/java/com/vr61v/model/request/UpdateOrderRequest.java
+++ order/src/main/java/com/vr61v/model/request/UpdateOrderRequest.java
@@ -1,14 +1,23 @@
 package com.vr61v.model.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.Calendar;
 import java.util.Map;
 import java.util.UUID;
 
 public record UpdateOrderRequest(
+
         UUID userId,
+
         UUID restaurantId,
+
         Calendar date,
+
+        @NotBlank
         String comment,
+
         Map<UUID, Integer>products
+
 ) {
 }
